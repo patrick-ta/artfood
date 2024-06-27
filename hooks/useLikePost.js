27e03148b450a26053396870166528c82012abb0
patrick-ta <patrick.Ta451@gmail.com>
2024-06-27 17:53:50 -0400

    Fixed bug where profile page would not display for unauthenticated users

--- hooks/useLikePost.js
+++ hooks/useLikePost.js
@@ -3,34 +3,49 @@ import { arrayRemove, arrayUnion, doc, updateDoc } from "firebase/firestore";
 import { firestore } from "../firebase/firebase";
 
 const useLikePost = (post) => {
-    const uid = JSON.parse(localStorage.getItem("user-info")).uid;
-    const [isUpdating, setIsUpdating] = useState(false);
-    const [likes, setLikes] = useState(post.likes.length);
-    const [isLiked, setIsLiked] = useState(post.likes.includes(uid));
+    const userInfoString = localStorage.getItem("user-info");
+    const userInfo = userInfoString ? JSON.parse(userInfoString) : null;
 
-    const handleLikePost = async () => {
-        if (isUpdating) return;
-        if (!uid) return;
+    if (userInfo !== null) {
+        console.log("test")
+        const [isUpdating, setIsUpdating] = useState(false);
+        const [likes, setLikes] = useState(post.likes.length);
+        const [isLiked, setIsLiked] = useState(post.likes.includes(userInfo.uid));
 
-        setIsUpdating(true);
-        try {
-            const postRef = doc(firestore, "posts", post.id);
-            await updateDoc(postRef, {
-                likes: isLiked ? arrayRemove(uid) : arrayUnion(uid),
-            });
+        const handleLikePost = async () => {
+            if (isUpdating) return;
 
-            setIsLiked(!isLiked);
-            isLiked ? setLikes(likes - 1) : setLikes(likes + 1);
+            setIsUpdating(true);
+            try {
+                const postRef = doc(firestore, "posts", post.id);
+                await updateDoc(postRef, {
+                    likes: isLiked ? arrayRemove(userInfo.uid) : arrayUnion(userInfo.uid),
+                });
+
+                setIsLiked(!isLiked);
+                isLiked ? setLikes(likes - 1) : setLikes(likes + 1);
+            }
+            catch (error) {
+                console.log(error);
+            }
+            finally {
+                setIsUpdating(false);
+            }
+        };
+
+        return { isLiked, likes, handleLikePost, isUpdating };
+    }
+    else {
+        console.log("test2")
+        const [isUpdating, setIsUpdating] = useState(false);
+        const [likes, setLikes] = useState(post.likes.length);
+        const [isLiked, setIsLiked] = useState(false);
+        const handleLikePost = async () => {
+            console.log("you must be signed in to like posts");
         }
-        catch (error) {
-            console.log(error);
-        }
-        finally {
-            setIsUpdating(false);
-        }
-    };
-
-    return { isLiked, likes, handleLikePost, isUpdating };
+        return { isLiked, likes, handleLikePost, isUpdating };
+    }
+    
 }
 
 export default useLikePost
