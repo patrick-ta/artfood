d46780d11e7556eea6a22cdcf0a8b0085a9c836b
patrick-ta <patrick.Ta451@gmail.com>
2024-06-29 19:52:24 -0400

    added real time updating to comments

--- hooks/useGetComments.js
+++ hooks/useGetComments.js
@@ -1,5 +1,61 @@
 import { useEffect, useState } from "react";
 import { firestore } from "../firebase/firebase";
+import { collection, query, where, onSnapshot } from "firebase/firestore";
+
+const useGetComments = (postData) => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [commentData, setCommentData] = useState([]);
+
+    console.log(postData);
+
+    useEffect(() => {
+        let unsubscribe = () => {};
+
+        const getCommentData = async () => {
+            setIsLoading(true);
+            try {
+                const postsRef = collection(firestore, "posts");
+                const q = query(postsRef, where("id", "==", postData.id));
+
+                unsubscribe = onSnapshot(q, (snapshot) => {
+                    let comments = [];
+                    snapshot.forEach((doc) => {
+                        comments = doc.data().comments;
+                    });
+
+                    if (comments.length === 0) return setCommentData(null);
+
+                    comments.sort((a, b) => b.createdAt - a.createdAt);
+
+                    setCommentData(comments);
+                });
+                
+            }
+            catch (error) {
+                console.log(error);
+            }
+            finally {
+                setIsLoading(false);
+            }
+        }
+
+        getCommentData();
+
+        return () => unsubscribe();
+    }, [postData]);
+
+    return {isLoading, commentData};
+}
+
+export default useGetComments
+
+
+/*
+OLD CODE
+here just in case i wanna redo real-time functionality
+
+import { useEffect, useState } from "react";
+import { firestore } from "../firebase/firebase";
 import { collection, getDocs, query, where } from "firebase/firestore";
 
 const useGetComments = (postData) => {
@@ -44,3 +100,5 @@ const useGetComments = (postData) => {
 }
 
 export default useGetComments
+
+*/
